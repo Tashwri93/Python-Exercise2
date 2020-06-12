2c4f2c8fd4419e7a2c9be116ff9ba75fbe057bb5
Tashan <tashwri93@hotmail.co.uk>
2020-06-12 15:31:46 +0100

    Add files via upload

---
+++ excercise2.8.py
@@ -0,0 +1,8 @@
+print(5+3)
+print(10-2)
+print(4*2)
+print(16/2)
+
+fav_num = 7
+
+print("Your favourite number is " + str(fav_num))
---
+++ exercise2.py
@@ -0,0 +1,14 @@
+name = "tashan"
+print("Hello, " + name.title() + " " + "would you like to learn some Python today?")
+      
+print("Hello, " + name.upper() + " " + "would you like to learn some Python today?")
+
+print("Hello, " + name.lower() + " " + "would you like to learn some Python today?")
+
+print("Albert Einstein once said, “A person who never made a mistake \nnever tried anything new.”")
+
+famous_person = "Albert Einstein"
+
+message = famous_person + " once said, “A person who never made a mistake \nnever tried anything new.”"
+
+print(message)
